302fe6ddbdb3b01f8bdd912427c9a80da0c8d0a0
Lake Heckaman <lakeheckaman@gmail.com>
2022-12-02 08:10:27 -0500

    update ui

--- script.js
+++ script.js
@@ -246,7 +246,7 @@ function startGUI () {
     gui.add(config, 'CURL', 0, 50).name('vorticity').step(1);
     gui.add(config, 'SPLAT_RADIUS', 0.01, 1.0).name('splat radius');
     gui.add(config, 'SHADING').name('shading').onFinishChange(updateKeywords);
-    gui.add(config, 'COLORFUL').name('colorful');
+    // gui.add(config, 'COLORFUL').name('colorful');
     gui.add(config, 'PAUSED').name('paused').listen();
 
     let mapFolder = gui.addFolder('Maps');
@@ -258,22 +258,23 @@ function startGUI () {
         splatStack.push(parseInt(Math.random() * 20) + 5);
     } }, 'fun').name('Random splats');
 
-    let bloomFolder = gui.addFolder('Bloom');
-    bloomFolder.add(config, 'BLOOM').name('enabled').onFinishChange(updateKeywords);
-    bloomFolder.add(config, 'BLOOM_INTENSITY', 0.1, 2.0).name('intensity');
-    bloomFolder.add(config, 'BLOOM_THRESHOLD', 0.0, 1.0).name('threshold');
+    // let bloomFolder = gui.addFolder('Bloom');
+    // bloomFolder.add(config, 'BLOOM').name('enabled').onFinishChange(updateKeywords);
+    // bloomFolder.add(config, 'BLOOM_INTENSITY', 0.1, 2.0).name('intensity');
+    // bloomFolder.add(config, 'BLOOM_THRESHOLD', 0.0, 1.0).name('threshold');
 
-    let sunraysFolder = gui.addFolder('Sunrays');
-    sunraysFolder.add(config, 'SUNRAYS').name('enabled').onFinishChange(updateKeywords);
-    sunraysFolder.add(config, 'SUNRAYS_WEIGHT', 0.3, 1.0).name('weight');
+    // let sunraysFolder = gui.addFolder('Sunrays');
+    // sunraysFolder.add(config, 'SUNRAYS').name('enabled').onFinishChange(updateKeywords);
+    // sunraysFolder.add(config, 'SUNRAYS_WEIGHT', 0.3, 1.0).name('weight');
 
     let captureFolder = gui.addFolder('Capture');
     captureFolder.addColor(config, 'BACK_COLOR').name('background color');
     captureFolder.add(config, 'TRANSPARENT').name('transparent');
     captureFolder.add({ fun: captureScreenshot }, 'fun').name('take screenshot');
 
+    //create a function to assign to a button, here linking my github
     let github = gui.add({ fun : () => {
-        window.open('https://github.com/lakeheck/WebGL-Fluid-Simulation');
+        window.open('https://github.com/lakeheck/Fluid-Simulation-WebGL');
         ga('send', 'event', 'link button', 'github');
     } }, 'fun').name('Github');
     github.__li.className = 'cr function bigFont';
@@ -281,37 +282,6 @@ function startGUI () {
     let githubIcon = document.createElement('span');
     github.domElement.parentElement.appendChild(githubIcon);
     githubIcon.className = 'icon github';
-
-    // //can create a function to assign to a button
-    // let twitter = gui.add({ fun : () => {
-    //     ga('send', 'event', 'link button', 'twitter');
-    //     window.open('https://twitter.com/PavelDoGreat');
-    // } }, 'fun').name('Twitter');
-    // twitter.__li.className = 'cr function bigFont';
-    // twitter.__li.style.borderLeft = '3px solid #8C8C8C';
-    // let twitterIcon = document.createElement('span');
-    // twitter.domElement.parentElement.appendChild(twitterIcon);
-    // twitterIcon.className = 'icon twitter';
-
-    // let discord = gui.add({ fun : () => {
-    //     ga('send', 'event', 'link button', 'discord');
-    //     window.open('https://discordapp.com/invite/CeqZDDE');
-    // } }, 'fun').name('Discord');
-    // discord.__li.className = 'cr function bigFont';
-    // discord.__li.style.borderLeft = '3px solid #8C8C8C';
-    // let discordIcon = document.createElement('span');
-    // discord.domElement.parentElement.appendChild(discordIcon);
-    // discordIcon.className = 'icon discord';
-
-    // let app = gui.add({ fun : () => {
-    //     ga('send', 'event', 'link button', 'app');
-    //     window.open('http://onelink.to/5b58bn');
-    // } }, 'fun').name('Check out mobile app');
-    // app.__li.className = 'cr function appBigFont';
-    // app.__li.style.borderLeft = '3px solid #00FF7F';
-    // let appIcon = document.createElement('span');
-    // app.domElement.parentElement.appendChild(appIcon);
-    // appIcon.className = 'icon app';
 
     if (isMobile())
         gui.close();
